85b9abb177c2e78c03f15c8a95503ee1e028edca
Favourezeugwa <favourezeugwa@gmail.com>
2022-05-10 10:41:14 +0100

    Fix linter errors

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-const toolbar = document.getElementById("toolbar")
-const menuimage = document.getElementById("menuimage")
-menuimage.addEventListener("click", function() {
-    // toolbar.classList.add("mobile-open")
-    toolbar.classList.toggle("mobile-open")
-})
-
-document.querySelectorAll("#header-nav a").forEach(element => {
-    element.addEventListener('click', () => {
-        toolbar.classList.remove("mobile-open")
-    })
-})
-
-
-const menuClose = document.getElementById("menu-close")
-menuClose.addEventListener("click", function() {
-    toolbar.classList.remove("mobile-open")
-})
+const toolbar = document.getElementById('toolbar');
+const menuimage = document.getElementById('menuimage');
+menuimage.addEventListener('click', () => {
+  toolbar.classList.toggle('mobile-open');
+});
+document.querySelectorAll('#header-nav a').forEach((element) => {
+  element.addEventListener('click', () => {
+    toolbar.classList.remove('mobile-open');
+  });
+});
+const menuClose = document.getElementById('menu-close');
+menuClose.addEventListener('click', () => {
+  toolbar.classList.remove('mobile-open');
+});
