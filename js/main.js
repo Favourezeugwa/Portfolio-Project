22ab8b500e83904d2312faea8e65fe54395f7c81
Favourezeugwa <favourezeugwa@gmail.com>
2022-05-13 19:01:51 +0100

    Create variables for client side validation and modify html form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// The mobile menu nav bar for popup starts
 const toolbar = document.getElementById('toolbar');
 const menuimage = document.getElementById('menuimage');
 menuimage.addEventListener('click', () => {
@@ -12,3 +13,9 @@ const menuClose = document.getElementById('menu-close');
 menuClose.addEventListener('click', () => {
   toolbar.classList.remove('mobile-open');
 });
+// Ends
+
+// client side validation starts
+const contactForm = document.getElementById('form');
+const email = document.getElementById('email');
+const formButton = document.getElementById('form-button');
